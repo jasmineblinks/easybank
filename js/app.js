d68e2459fa611a81ebc1aecad14ffb90b4c04943
faith <puenehfaith@gmail.com>
2020-09-25 00:22:08 +0100

    updated

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+
+
+function openLink(){
+  document.getElementById('la-links').style.display ="block";
+  document.getElementById('open').style.display = "none";
+  document.getElementById('close').style.display = "block";
+
+}
+ function closeLink(){
+   document.getElementById('la-links').style.display = "none";
+   document.getElementById('open').style.display = "block"
+   document.getElementById('close').style.display = "none";
+ }
